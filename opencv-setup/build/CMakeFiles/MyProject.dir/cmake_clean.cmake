8e1a9d64fbdc6256d38937909a21f1c758ad2a7e
Elliot Arledge <59771030+Infatoshi@users.noreply.github.com>
2023-02-19 23:59:36 -0700

    Add files via upload

---
+++ opencv-setup/build/CMakeFiles/MyProject.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyProject.dir/tutorial.cpp.o"
+  "CMakeFiles/MyProject.dir/tutorial.cpp.o.d"
+  "MyProject"
+  "MyProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
